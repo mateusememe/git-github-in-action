3c8214d9f77d60fbf879a8f8768f4819b2e28702
Diogo <diogopetroski@gmail.com>
2023-10-27 17:21:55 -0300

    exercício

--- codigos/codigo-mateus.cpp
+++ codigos/codigo-mateus.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
 int main() {
-	printf("Ola mundo!");
+	printf("Ola mundo!Ola mundoOla mundoOla mundoOla mundoOla mundoOla mundoOla mundoOla mundoOla mundoOla mundoOla mundo");
 	return 0;
 }
