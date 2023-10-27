df36e68f90c401a3887cc01bda3d6a9569f79e95
dennercyc <dennereduardo70@gmail.com>
2023-10-27 17:17:34 -0300

    :sparkles: feat: adicionado função de numero fatorial

--- codigos/codigo-mateus.cpp
+++ codigos/codigo-mateus.cpp
@@ -1,6 +1,24 @@
 #include<stdio.h>
 
+int fat (int x)
+{
+	if (x == 0 || x == 1)
+		return 1;
+	else 
+		return x * fat(x-1);
+}
+
+
 int main() {
+	
+	int n;
 	printf("Ola mundo!");
+	printf("Numero Fatorial:");
+	scanf("%d", &n);
+	printf("%d", fat(n));
+	
 	return 0;
 }
+
+
+
